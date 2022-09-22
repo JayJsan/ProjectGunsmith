8710cd3cc246be83fe8d61ea52dd1f7191a5fb22
jsan502 <job.santos924@gmail.com>
2022-09-22 15:42:58 +1200

    Added getcomponents for zones

--- Assets/Scripts/UI/DragAndDrop.cs
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -10,10 +10,32 @@ public class DragAndDrop : MonoBehaviour
     private DropZone dz;
     private Shooting playerShooting;
 
+    #region DROP ZONES
+    private DropZone barrelZone;
+    private DropZone magazineZone;
+    private DropZone triggerZone;
+    private DropZone sightZone;
+    private DropZone stockZone;
+    private DropZone specialZone;
+    #endregion
+
+    public string partType;
+
     private void Start()
     {
+        #region DROP ZONE GET COMPONENT
+        barrelZone = GameObject.Find("CM vcam1/WeaponHolder/BarrelHolder").GetComponent<DropZone>();
+        magazineZone = GameObject.Find("CM vcam1/WeaponHolder/MagazineHolder").GetComponent<DropZone>();
+        triggerZone = GameObject.Find("CM vcam1/WeaponHolder/TriggerHolder").GetComponent<DropZone>();
+        sightZone = GameObject.Find("CM vcam1/WeaponHolder/SightHolder").GetComponent<DropZone>();
+        stockZone = GameObject.Find("CM vcam1/WeaponHolder/StockHolder").GetComponent<DropZone>();
+        specialZone = GameObject.Find("CM vcam1/WeaponHolder/SpecialHolder").GetComponent<DropZone>();
+
+        #endregion
+
         dz = GameObject.FindWithTag("DropZone").GetComponent<DropZone>();
         playerShooting = GameObject.Find("Player").GetComponent<Shooting>();
+        partType = this.gameObject.name;
         playerShooting.enableShoot = true;
     }
 
@@ -36,20 +58,9 @@ public class DragAndDrop : MonoBehaviour
             transform.localPosition = Vector3.zero;
         }
 
-        // Check what type of drop zone gun part is hovering over.
-        // If drop zone type == gun part type, lock position to respective drop zone
-        // If there is an existing gun part, drop existing gun part and lock new gun part onto gun part holder
-        // If gun part hovering over actual drop zone then unlock gun part from position and drop gun part
-        // Use switch case(?)
+        CheckGunType();
 
-        // Gun part should be a prefab(?) - scale down when dropped and scale up when picked up
-        // Gun part should have a reference to a scriptable object with its respective stats
-        // Hovering gun part in its dropped state should bring up a tooltip
-        // Picking it up should disable tooltip and scale to original size
 
-        // Statmanager should check every gun part holder for its stats
-        // Change each weapons stats accordingly
-        // Gun sprite should change accordingly
     }
 
     // Update is called once per frame
@@ -60,5 +71,23 @@ public class DragAndDrop : MonoBehaviour
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             transform.Translate(mousePosition);
         }
+    }
+
+    void CheckGunType()
+    {
+        // Check what type of drop zone gun part is hovering over.
+        // If drop zone type == gun part type, lock position to respective drop zone
+        // If there is an existing gun part, drop existing gun part and lock new gun part onto gun part holder
+        // If gun part hovering over actual drop zone then unlock gun part from position and drop gun part
+        // Use switch case(?)
+        
+        // Gun part should be a prefab(?) - scale down when dropped and scale up when picked up
+        // Gun part should have a reference to a scriptable object with its respective stats
+        // Hovering gun part in its dropped state should bring up a tooltip
+        // Picking it up should disable tooltip and scale to original size
+
+        // Statmanager should check every gun part holder for its stats
+        // Change each weapons stats accordingly
+        // Gun sprite should change accordingly
     }
 }
