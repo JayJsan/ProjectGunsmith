ed68c698457a6e021f3d589c10cf3fe396d577a2
jsan502 <job.santos924@gmail.com>
2022-09-23 20:50:34 +1200

    Added implementation of partType and zoneTypes

--- Assets/Scripts/UI/DragAndDrop.cs
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    public enum partTypes { Barrel, Magazine, Trigger, Sight, Stock, Special}
     private bool isDragging;
 
     [SerializeField]
@@ -11,31 +12,36 @@ public class DragAndDrop : MonoBehaviour
     private Shooting playerShooting;
 
     #region DROP ZONES
-    private DropZone barrelZone;
-    private DropZone magazineZone;
-    private DropZone triggerZone;
-    private DropZone sightZone;
-    private DropZone stockZone;
-    private DropZone specialZone;
+    public DropZone barrelZone;
+    public DropZone magazineZone;
+    public DropZone triggerZone;
+    public DropZone sightZone;
+    public DropZone stockZone;
+    public DropZone specialZone;
     #endregion
 
-    public string partType;
-
+    public partTypes partType;
     private void Start()
     {
         #region DROP ZONE GET COMPONENT
-        barrelZone = GameObject.Find("CM vcam1/WeaponHolder/BarrelHolder").GetComponent<DropZone>();
-        magazineZone = GameObject.Find("CM vcam1/WeaponHolder/MagazineHolder").GetComponent<DropZone>();
-        triggerZone = GameObject.Find("CM vcam1/WeaponHolder/TriggerHolder").GetComponent<DropZone>();
-        sightZone = GameObject.Find("CM vcam1/WeaponHolder/SightHolder").GetComponent<DropZone>();
-        stockZone = GameObject.Find("CM vcam1/WeaponHolder/StockHolder").GetComponent<DropZone>();
-        specialZone = GameObject.Find("CM vcam1/WeaponHolder/SpecialHolder").GetComponent<DropZone>();
+        barrelZone = GameObject.Find("CMvcam1/WeaponHolder/BarrelHolder").GetComponent<DropZone>();
+        magazineZone = GameObject.Find("CMvcam1/WeaponHolder/MagazineHolder").GetComponent<DropZone>();
+        triggerZone = GameObject.Find("CMvcam1/WeaponHolder/TriggerHolder").GetComponent<DropZone>();
+        sightZone = GameObject.Find("CMvcam1/WeaponHolder/SightHolder").GetComponent<DropZone>();
+        stockZone = GameObject.Find("CMvcam1/WeaponHolder/StockHolder").GetComponent<DropZone>();
+        specialZone = GameObject.Find("CMvcam1/WeaponHolder/SpecialHolder").GetComponent<DropZone>();
 
         #endregion
 
         dz = GameObject.FindWithTag("DropZone").GetComponent<DropZone>();
         playerShooting = GameObject.Find("Player").GetComponent<Shooting>();
-        partType = this.gameObject.name;
+        foreach (DragAndDrop.partTypes partType in System.Enum.GetValues(typeof(DragAndDrop.partTypes)))
+        {
+            if (this.gameObject.name.Contains(partType.ToString()))
+            {
+                this.partType = partType;
+            }
+        }
         playerShooting.enableShoot = true;
     }
 
@@ -53,11 +59,13 @@ public class DragAndDrop : MonoBehaviour
         dz.GetComponent<SpriteRenderer>().enabled = false;
         playerShooting.enableShoot = true;
         // If player is hovering over drop zone, remove gunparts ability to return to gun holder and drop
+        /* locks sprite
         if (!dz.hoveringOver)
         {
             transform.localPosition = Vector3.zero;
         }
 
+        */
         CheckGunType();
 
 
@@ -77,6 +85,47 @@ public class DragAndDrop : MonoBehaviour
     {
         // Check what type of drop zone gun part is hovering over.
         // If drop zone type == gun part type, lock position to respective drop zone
+        switch (partType)
+        {
+            case partTypes.Barrel:
+                if (barrelZone.hoveringOver)
+                {
+                    transform.localPosition = Vector3.zero;
+                }
+                break;
+            case partTypes.Magazine:
+                if (magazineZone.hoveringOver)
+                {
+                    transform.localPosition = Vector3.zero;
+                }
+                break;
+            case partTypes.Sight:
+                if (sightZone.hoveringOver)
+                {
+                    transform.localPosition = Vector3.zero;
+                }
+                break;
+            case partTypes.Special:
+                if (specialZone.hoveringOver)
+                {
+                    transform.localPosition = Vector3.zero;
+                }
+                break;
+            case partTypes.Stock:
+                if (stockZone.hoveringOver)
+                {
+                    transform.localPosition = Vector3.zero;
+                }
+                break;
+            case partTypes.Trigger:
+                if (triggerZone.hoveringOver)
+                {
+                    transform.localPosition = Vector3.zero; 
+                }
+                break;
+        }
+
+
         // If there is an existing gun part, drop existing gun part and lock new gun part onto gun part holder
         // If gun part hovering over actual drop zone then unlock gun part from position and drop gun part
         // Use switch case(?)
@@ -89,5 +138,9 @@ public class DragAndDrop : MonoBehaviour
         // Statmanager should check every gun part holder for its stats
         // Change each weapons stats accordingly
         // Gun sprite should change accordingly
+    }
+    void DropGunPart(partTypes zoneType)
+    {
+
     }
 }
--- Assets/Scripts/UI/DropZone.cs
+++ Assets/Scripts/UI/DropZone.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class DropZone : MonoBehaviour
 {
     public bool hoveringOver;
-    public string holderName;
-
+    public DragAndDrop.partTypes zoneType;
     private void Start()
     {
         GetComponent<SpriteRenderer>().enabled = false;
-        holderName = this.gameObject.name;
+        // Checks the name of the zoneholder and sees if it matches any of the enums (magazine)
+        // and sets the zonetype to that part type.
+        foreach (DragAndDrop.partTypes partType in System.Enum.GetValues(typeof(DragAndDrop.partTypes)))
+        {
+            if (this.gameObject.name.Contains(partType.ToString()))
+            {
+                zoneType = partType;
+            }
+        }
+
     }
     private void OnMouseOver()
     {
